39a4bebc2e31d0fc19dbb92758d694bb9ea4a40e
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-04 23:22:03 +0200

    Refactor test name for strcpy

--- test_libft/test/test_strcpy.c
+++ test_libft/test/test_strcpy.c
@@ -9,21 +9,21 @@ void	tearDown(void)
 {
 }
 
-void	test_when_src_equal_dst(void)
+void	test_when_src_same_length_as_dst(void)
 {
 	const char *src = "abc";
 	char *dst = (char *)malloc((strlen(src) + 1) * sizeof(char));
 	TEST_ASSERT_EQUAL_STRING(strcpy(dst, src), ft_strcpy(dst, src));
 }
 
-void	test_when_src_larger_than_dst(void)
+void	test_when_src_longer_than_dst(void)
 {
 	const char *src = "abcd";
 	char *dst = (char *)malloc((strlen(src) - 1) * sizeof(char));
 	TEST_ASSERT_EQUAL_STRING(strcpy(dst, src), ft_strcpy(dst, src));
 }
 
-void	test_when_src_less_than_dst(void)
+void	test_when_src_shorter_than_dst(void)
 {
 	const char *src = "abcd";
 	char *dst = (char *)malloc((strlen(src) + 3) * sizeof(char));
