eb4433b6dcbbb9820fec88c043805e85f033a136
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-04 19:11:13 +0200

    Add tests for ft_strlen

--- test_libft/test/test_hbui.c
+++ test_libft/test/test_hbui.c
@@ -1,14 +1,24 @@
 #include "libft.h"
 #include "unity.h"
 
-void setUp(void)
+void	setUp(void)
 {
 }
 
-void tearDown(void)
+void	tearDown(void)
 {
 }
-void test_strlen(void)
+void	test_strlen_whenAbc_then3(void)
 {
 	TEST_ASSERT_EQUAL(3, ft_strlen("abc"));
+}
+
+void	test_strlen_whenNull_then0(void)
+{
+	TEST_ASSERT_EQUAL(0, ft_strlen(0));
+}
+
+void	test_strlen_whenEmpty_then0(void)
+{
+	TEST_ASSERT_EQUAL(0, ft_strlen(""));
 }
