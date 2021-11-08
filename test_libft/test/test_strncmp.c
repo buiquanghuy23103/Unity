378e34a47fcab2a57759fcb223d13b39bfbc6bea
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-08 19:50:42 +0200

    Add tests for strncmp

---
+++ test_libft/test/test_strncmp.c
@@ -0,0 +1,32 @@
+#include "libft.h"
+#include "unity.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+void	test_when_n_larger_than_s1(void)
+{
+	const char *s1 = "abc";
+	const char *s2 = "abc";
+	size_t n = 4;
+	TEST_ASSERT_EQUAL(strncmp(s1, s2, n), ft_strncmp(s1, s2, n));
+}
+void	test_when_n_zero(void)
+{
+	const char *s1 = "abc";
+	const char *s2 = "abc";
+	size_t n = 0;
+	TEST_ASSERT_EQUAL(strncmp(s1, s2, n), ft_strncmp(s1, s2, n));
+}
+void	test_when_s1_partially_equal_s2(void)
+{
+	const char *s1 = "abcdef";
+	const char *s2 = "abcxyz";
+	size_t n = 3;
+	TEST_ASSERT_EQUAL(strncmp(s1, s2, n), ft_strncmp(s1, s2, n));
+}
