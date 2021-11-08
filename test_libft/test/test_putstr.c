9ac7bf340438196032547e245e7f9a65a25a928a
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-08 19:50:22 +0200

    Add tests for putstr

---
+++ test_libft/test/test_putstr.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+#include "unity.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+void	test_whenNull_thenNonError(void)
+{
+	ft_putstr_fd(NULL, 1);
+	ft_putstr(NULL);
+	ft_putendl_fd(NULL, 1);
+	ft_putendl(NULL);
+	TEST_ASSERT_EQUAL(1, 1);
+}
