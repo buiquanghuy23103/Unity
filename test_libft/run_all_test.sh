7c5d27a67f7fc09098f6615705814dc73cac4bab
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-11 19:56:36 +0200

    Only show errors and failures in test report

--- test_libft/run_all_test.sh
+++ test_libft/run_all_test.sh
@@ -1,5 +1,13 @@
-make -s
-for f in targets/* ; do
-	echo "$f"
-	./$f
-done
+produce_test_files() {
+	make -s
+}
+
+execute_test_files() {
+	for f in targets/* ; do
+		echo "$f"
+		./$f
+	done
+}
+
+produce_test_files
+execute_test_files | grep -v -e ":PASS" -e "------" -e "OK" -e "^$" -e "0 Failures 0 Ignored"
