036380ddb3d897fde4349dfe0e5ecf6aadb5bfd8
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-12 19:17:49 +0200

    Add tests for negative fd

--- test_libft/test/test_putstr.c
+++ test_libft/test/test_putstr.c
@@ -16,3 +16,11 @@ void	test_whenNull_thenNonError(void)
 	ft_putendl(NULL);
 	TEST_ASSERT_EQUAL(1, 1);
 }
+
+void	test_when_negative_fd_then_non_error(void)
+{
+	ft_putchar_fd('a', -1);
+	ft_putstr_fd("abc", -1);
+	ft_putendl_fd("abc", -1);
+	TEST_ASSERT_EQUAL(1, 1);
+}
