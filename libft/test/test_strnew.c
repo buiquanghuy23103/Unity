8796bd0dea20b50e00273deb7cc9ebc6f63ca6b9
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-22 18:30:10 +0200

    Add tests for strnew

---
+++ libft/test/test_strnew.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include "unity.h"
+#include "ctype.h"
+#include "string.h"
+#include "stdlib.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+void	test_when_size_0_then_do_nothing(void)
+{
+	ft_strnew(0);
+	TEST_PASS();
+}
+
+void	test_when_size_6_then_empty_new_str(void)
+{
+	char	*mem;
+	size_t	size;
+	size_t	i;
+
+	size = 6;
+	i = 0;
+	mem = ft_strnew(size);
+	while (i < size)
+	{
+		TEST_ASSERT_EQUAL_CHAR('\0', mem[i]);
+		i++;
+	}
+}
