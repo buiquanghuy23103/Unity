84deef121429ecfb2d191ee33ea13ff39ee232fd
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-25 21:29:41 +0200

    Fix memory leaks in tests for itoa

--- libft/test/test_itoa.c
+++ libft/test/test_itoa.c
@@ -9,28 +9,32 @@ void	tearDown(void)
 {
 }
 
+void	TEST_ASSERT_EQUAL_ITOA(int num, char *expected)
+{
+	char	*actual;
+
+	actual = ft_itoa(num);
+	TEST_ASSERT_EQUAL_STRING(expected, actual);
+	free(actual);
+}
+
 void	test_when_negative_number(void)
 {
-	int i = -4235;
-	TEST_ASSERT_EQUAL_STRING("-4235", ft_itoa(i));
+	TEST_ASSERT_EQUAL_ITOA(-4235, "-4235");
 }
 void	test_when_zero(void)
 {
-	int i = 0;
-	TEST_ASSERT_EQUAL_STRING("0", ft_itoa(i));
+	TEST_ASSERT_EQUAL_ITOA(0, "0");
 }
 void	test_when_positive(void)
 {
-	int i = 4235;
-	TEST_ASSERT_EQUAL_STRING("4235", ft_itoa(i));
+	TEST_ASSERT_EQUAL_ITOA(4235, "4235");
 }
 void	test_when_min_int(void)
 {
-	int i = -2147483648;
-	TEST_ASSERT_EQUAL_STRING("-2147483648", ft_itoa(i));
+	TEST_ASSERT_EQUAL_ITOA(-2147483648,"-2147483648");
 }
 void	test_when_max_int(void)
 {
-	int i = 2147483647;
-	TEST_ASSERT_EQUAL_STRING("2147483647", ft_itoa(i));
+	TEST_ASSERT_EQUAL_ITOA(2147483647, "2147483647");
 }
