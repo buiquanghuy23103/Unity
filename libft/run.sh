e07841abf967d77a4f5ddfa6b31d4e1d0513e63c
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-25 21:17:41 +0200

    Filter out unecessary error messages for memory leaks

--- libft/run.sh
+++ libft/run.sh
@@ -5,9 +5,14 @@ produce_test_files() {
 execute_test_files() {
 	for f in targets/* ; do
 		echo "$f"
-		./$f
+		leaks -q --atExit -- ./$f
 	done
 }
 
 produce_test_files
-execute_test_files | grep -v -e ":PASS" -e "------" -e "OK" -e "^$" -e "0 Failures 0 Ignored"
+execute_test_files 2>&1 \
+| grep -v -e ":PASS" -e "------" -e "OK" -e "^$" -e "0 Failures 0 Ignored" \
+| grep -v -e "0 leaks for 0 total leaked bytes." \
+| grep -v -e "is not debuggable. Due to security restrictions," \
+| grep -v -e "leaks Report Version: 4.0" \
+| grep -v -e "nodes malloced for"
