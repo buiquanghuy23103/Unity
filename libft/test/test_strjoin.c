87eb342a74563fe6b5b4d8dc0abc765ca017c7d7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-25 20:22:47 +0200

    Prevent memory leaks from tests of strjoin

--- libft/test/test_strjoin.c
+++ libft/test/test_strjoin.c
@@ -16,38 +16,50 @@ void	test_when_prefix_empty_then_equal_suffix(void)
 {
 	char	*prefix;
 	char	*suffix;
+	char	*actual;
 
 	prefix = "";
 	suffix = "Hello world!\n";
-	TEST_ASSERT_EQUAL_STRING(suffix, ft_strjoin(prefix, suffix));
+	actual = ft_strjoin(prefix, suffix);
+	TEST_ASSERT_EQUAL_STRING(suffix, actual);
+	free(actual);
 }
 
 void	test_when_suffix_empty_then_equal_prefix(void)
 {
 	char	*prefix;
 	char	*suffix;
+	char	*actual;
 
 	prefix = "Hello world!\n";
 	suffix = "";
-	TEST_ASSERT_EQUAL_STRING(prefix, ft_strjoin(prefix, suffix));
+	actual = ft_strjoin(prefix, suffix);
+	TEST_ASSERT_EQUAL_STRING(prefix, actual);
+	free(actual);
 }
 
 void	test_when_both_prefix_suffix_empty(void)
 {
 	char	*prefix;
 	char	*suffix;
+	char	*actual;
 
 	prefix = "";
 	suffix = "";
-	TEST_ASSERT_EMPTY(ft_strjoin(prefix, suffix));
+	actual = ft_strjoin(prefix, suffix);
+	TEST_ASSERT_EMPTY(actual);
+	free(actual);
 }
 
 void	test_when_both_prefix_suffix_non_empty(void)
 {
 	char	*prefix;
 	char	*suffix;
+	char	*actual;
 
 	prefix = "How are you? ";
 	suffix = "Well, I am fine.\n";
-	TEST_ASSERT_EQUAL_STRING("How are you? Well, I am fine.\n", ft_strjoin(prefix, suffix));
+	actual = ft_strjoin(prefix, suffix);
+	TEST_ASSERT_EQUAL_STRING("How are you? Well, I am fine.\n", actual);
+	free(actual);
 }
