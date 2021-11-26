4e878cc26c4a4053516e1c32f0543f06e816c4d9
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-26 16:23:29 +0200

    Update tests for memcpy

--- libft/test/test_memcpy.c
+++ libft/test/test_memcpy.c
@@ -12,28 +12,15 @@ void	tearDown(void)
 {
 }
 
-void	test_when_cpy_whole_str(void)
+void	test_when_cpy_then_identical(void)
 {
-	char	*src;
-	char	*dst;
+	char	b1[100];
+	char	b2[100];
 
-	src = "This is a test.\n";
-	dst = (char *)ft_memalloc((ft_strlen(src) + 1) * sizeof(char));
-	ft_memcpy(dst, src, 16);
-	dst[16] = '\0';
-	TEST_ASSERT_EQUAL_STRING(src, dst);
-}
+	memset(b1, 42, 100);
+	memset(b2, 65, 100);
 
-void	test_when_cpy_partially_str(void)
-{
-	const char	*src;
-	char	*actual;
-
-	src = "This is a test.\n";
-	actual = (char *)ft_memalloc((ft_strlen(src) + 1) * sizeof(char));
-	ft_memcpy(actual, src, 10);
-	actual[10] = '\0';
-
-	TEST_ASSERT_EQUAL_STRING("This is a ", actual);
-	free(actual);
+	ft_memcpy(b1, b2, 100);
+	TEST_ASSERT_EQUAL_INT(0, memcmp(b1, b2, 100));
+	TEST_ASSERT_EQUAL_INT(0, memcmp(b1, ft_memcpy(b1, b2, 100), 100));
 }
