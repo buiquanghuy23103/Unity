1f022ab55c14b901d8620895d3841ea68e22abe8
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-29 22:17:19 +0200

    Add color to test report

--- libft/run.sh
+++ libft/run.sh
@@ -1,14 +1,17 @@
+C_CYAN='\033[0;36m'
+C_CLEAR='\033[0m'
+
 produce_test_files() {
 	make -s
 }
 
 execute_test_files() {
 	for f in targets/* ; do
-		echo "$f"
 		leaks -q --atExit -- ./$f
 	done
 }
 
+printf "${C_CYAN}-------STARTING ALL UNIT TESTS-------${C_CLEAR}\n"
 produce_test_files
 execute_test_files 2>&1 \
 | grep -v -e ":PASS" -e "------" -e "OK" -e "^$" -e "0 Failures 0 Ignored" \
@@ -16,3 +19,4 @@ execute_test_files 2>&1 \
 | grep -v -e "is not debuggable. Due to security restrictions," \
 | grep -v -e "leaks Report Version: 4.0" \
 | grep -v -e "nodes malloced for"
+printf "${C_CYAN}-------END ALL UNIT TESTS-------${C_CLEAR}\n"
