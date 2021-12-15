6fd602b0d46d5931c3fc0e1e16003418fa419adc
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-15 21:29:00 +0200

    first test for gnl

--- get_next_line/test/test_gnl.c
+++ get_next_line/test/test_gnl.c
@@ -1,4 +1,7 @@
+#include "get_next_line.h"
 #include "unity.h"
+#include <string.h>
+#include <stdlib.h>
 
 void	setUp(void)
 {
@@ -6,4 +9,29 @@ void	setUp(void)
 
 void	tearDown(void)
 {
+}
+
+void	test_medium_string_no_nl(void)
+{
+	char 	*line;
+	int		out;
+	int		p[2];
+	char 	*str;
+	int		gnl_ret;
+
+	str = (char *)malloc(1000 * 1000);
+	*str = '\0';
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	out = dup(1);
+	pipe(p);
+	dup2(p[1], 1);
+
+	write(1, str, strlen(str));
+	close(p[1]);
+	dup2(out, 1);
+	gnl_ret = get_next_line(p[0], &line);
+	TEST_ASSERT_EQUAL_STRING(str, line);
+	TEST_ASSERT_UINT_WITHIN(1, 0, -1);
 }
