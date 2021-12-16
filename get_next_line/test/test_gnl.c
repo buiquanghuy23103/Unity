e24cf6d8a784cafe9034ec52d30e44fee787cc2c
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-16 14:01:04 +0200

    add test of multiple fd for gnl

--- get_next_line/test/test_gnl.c
+++ get_next_line/test/test_gnl.c
@@ -482,3 +482,82 @@ void	test_line_of_16_no_nl(void)
 	TEST_ASSERT_EQUAL_STRING("mnopqrstuvwxyzab", line);
 	ft_strdel(&line);
 }
+
+void	test_multiple_fd(void)
+{
+	char	*line_fd0;
+	int		p_fd0[2];
+	int		fd0 = 0;
+	int		out_fd0 = dup(fd0);
+
+	char	*line_fd1;
+	int		p_fd1[2];
+	int		fd1 = 1;
+	int		out_fd1 = dup(fd1);
+
+	char	*line_fd2;
+	int		p_fd2[2];
+	int		fd2 = 2;
+	int		out_fd2 = dup(fd2);
+
+	char	*line_fd3;
+	int		p_fd3[2];
+	int		fd3 = 3;
+	int		out_fd3 = dup(fd3);
+
+	pipe(p_fd0);
+	dup2(p_fd0[1], fd0);
+	write(fd0, "aaa\nbbb\n", 8);
+	dup2(out_fd0, fd0);
+	close(p_fd0[1]);
+
+	pipe(p_fd1);
+	dup2(p_fd1[1], fd1);
+	write(fd1, "111\n222\n", 8);
+	dup2(out_fd1, fd1);
+	close(p_fd1[1]);
+
+	pipe(p_fd2);
+	dup2(p_fd2[1], fd2);
+	write(fd2, "www\nzzz\n", 8);
+	dup2(out_fd2, fd2);
+	close(p_fd2[1]);
+
+	pipe(p_fd3);
+	dup2(p_fd3[1], fd3);
+	write(fd3, "888\n999\n", 8);
+	dup2(out_fd3, fd3);
+	close(p_fd3[1]);
+
+	get_next_line(p_fd0[0], &line_fd0);
+	TEST_ASSERT_EQUAL_STRING("aaa", line_fd0);
+	ft_strdel(&line_fd0);
+
+	get_next_line(p_fd1[0], &line_fd1);
+	TEST_ASSERT_EQUAL_STRING("111", line_fd1);
+	ft_strdel(&line_fd1);
+
+	get_next_line(p_fd2[0], &line_fd2);
+	TEST_ASSERT_EQUAL_STRING("www", line_fd2);
+	ft_strdel(&line_fd2);
+
+	get_next_line(p_fd3[0], &line_fd3);
+	TEST_ASSERT_EQUAL_STRING("888", line_fd3);
+	ft_strdel(&line_fd3);
+
+	get_next_line(p_fd0[0], &line_fd0);
+	TEST_ASSERT_EQUAL_STRING("bbb", line_fd0);
+	ft_strdel(&line_fd0);
+
+	get_next_line(p_fd1[0], &line_fd1);
+	TEST_ASSERT_EQUAL_STRING("222", line_fd1);
+	ft_strdel(&line_fd1);
+
+	get_next_line(p_fd2[0], &line_fd2);
+	TEST_ASSERT_EQUAL_STRING("zzz", line_fd2);
+	ft_strdel(&line_fd2);
+
+	get_next_line(p_fd3[0], &line_fd3);
+	TEST_ASSERT_EQUAL_STRING("999", line_fd3);
+	ft_strdel(&line_fd3);
+}
