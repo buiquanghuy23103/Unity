ab4bcfbe9208612cb02656e88ad347ccc5565122
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-16 15:37:01 +0200

    run test with many buff size automatically

---
+++ get_next_line/test.sh
@@ -0,0 +1,9 @@
+make -s BUFF_SIZE=1
+make -s BUFF_SIZE=2
+make -s BUFF_SIZE=3
+make -s BUFF_SIZE=4
+make -s BUFF_SIZE=8
+make -s BUFF_SIZE=32
+make -s BUFF_SIZE=2056
+make -s BUFF_SIZE=9999
+make -s BUFF_SIZE=10000000
