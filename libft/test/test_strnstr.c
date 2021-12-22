68520b9e5852d6993a921fb312c83e786b971936
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-22 11:43:27 +0200

    remove redundant test for strnstr

--- libft/test/test_strnstr.c
+++ libft/test/test_strnstr.c
@@ -46,7 +46,7 @@ void	test_when_needle_found_but_len_too_small(void)
 	char	haystack[] = "Hey my name is Huy. What is you name?";
 	char	needle[] = "name is Huy.";
 
-	ASSERT_STRNSTR(haystack, needle, strlen(needle));
+	ASSERT_STRNSTR(haystack, needle, strlen(needle) - 1);
 }
 
 void	test_when_needle_found_but_len_just_big_enough(void)
@@ -55,12 +55,4 @@ void	test_when_needle_found_but_len_just_big_enough(void)
 	char	needle[] = "name is Huy.";
 
 	ASSERT_STRNSTR(haystack, needle, strlen(needle));
-}
-
-void	test_basic_input(void)
-{
-	char	haystack[] = "Hello les genw";
-	char	needle[] = "Hello";
-
-	ASSERT_STRNSTR(haystack, needle, 3);
 }
