b24b96ea7a4c7ea4465d1245fb4c9b50757e86d3
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-01-24 19:36:38 +0200

    setup test for ft_printf

---
+++ ft_printf/test/test_ft_printf.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+#include "unity.h"
+#include "ctype.h"
+#include "string.h"
+#include "stdlib.h"
+#include "ft_printf.h"
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+void	test0()
+{
+	TEST_PASS();
+}
