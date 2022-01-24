60422ad47eae98bc3b9ba5c0922443d8eaa3f6f9
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-01-24 21:29:04 +0200

    check if ft_printf can print normal string

--- ft_printf/test/test_ft_printf.c
+++ ft_printf/test/test_ft_printf.c
@@ -1,5 +1,6 @@
 #include "libft.h"
 #include "unity.h"
+#include "testft.h"
 #include "ctype.h"
 #include "string.h"
 #include "stdlib.h"
@@ -15,5 +16,12 @@ void	tearDown(void)
 
 void	test0()
 {
-	TEST_PASS();
+	int		fd = 0;
+	int		copy_out = 0;
+	char	expected[] = "Hello world!";
+
+	init_redirect(&fd, &copy_out);
+	ft_printf(expected);
+	reset_output(&copy_out);
+	TEST_ASSERT_FILE_CONTENT(expected, fd);
 }
