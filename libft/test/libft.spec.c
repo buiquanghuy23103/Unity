8ddfdf551c43732732da0ef6a7054c13d2e418e4
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-04-13 11:27:58 +0300

    add test for qsort

--- libft/test/libft.spec.c
+++ libft/test/libft.spec.c
@@ -1962,3 +1962,31 @@ void	test_ft_lst_pop_back2(void)
 	ft_lstdel(&lst, del_str_lst);
 	ft_lstdel(&ptr, del_str_lst);
 }
+
+// FT_QSORT
+void	test_ft_qsort1(void)
+{
+	int	actual[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1};
+	int	expected[] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	ft_qsort(actual, 11);
+	TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 11);
+}
+
+void	test_ft_qsort2(void)
+{
+	int	actual[] = {2, 1};
+	int	expected[] = {1, 2};
+
+	ft_qsort(actual, 2);
+	TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 2);
+}
+
+void	test_ft_qsort3(void)
+{
+	int	actual[] = {1};
+	int	expected[] = {1};
+
+	ft_qsort(actual, 1);
+	TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, 1);
+}
